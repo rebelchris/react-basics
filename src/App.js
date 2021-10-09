88d5dbfc7bca7a958275a0e1f2e03b61093f8879
Chris Bongers <chrisbongers@gmail.com>
2021-10-09 12:24:09 +0200

    Added a custom hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import { ThemeProvider } from './context/ThemeContext';
-import Toggle from './components/Toggle';
-import Paint from './components/Paint';
+import useMedia from './hooks/useMedia';
 
 function App() {
+  const isDesktop = useMedia('(min-width: 960px)');
+
   return (
-    <ThemeProvider>
-      <div className='App'>
-        <Paint />
-        <Toggle />
-      </div>
-    </ThemeProvider>
+    <div className='App'>
+      {isDesktop ? <h1>Desktop</h1> : <h1>Small screen</h1>}
+    </div>
   );
 }
 
